b8e7ea3aa6e7812427c7ee6d1c80bb18e2e0eec4
Faisal <peysal@gmail.com>
2018-03-21 00:26:20 +0800

    generate entity car for gateway

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,9 +1,11 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { GatewayCarModule } from './car/car.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
+        GatewayCarModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
